a62ad7250c5d8b49df375c0812f3c1cdb98d55b9
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2019-08-02 15:04:41 +0200

    Initial commit

---
+++ src/main/java/dev/foolen/kits/Kits.java
@@ -0,0 +1,12 @@
+package dev.foolen.kits;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Kits extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("onEnable has been invoked!");
+	}
+	
+}
