5835c7de1ca76cbf045789852a067806ed0b766c
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2019-08-02 16:22:38 +0200

    Fixed item color

--- src/main/java/dev/foolen/kits/events/listeners/InventoryClick.java
+++ src/main/java/dev/foolen/kits/events/listeners/InventoryClick.java
@@ -1,6 +1,5 @@
 package dev.foolen.kits.events.listeners;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -9,10 +8,7 @@ public class InventoryClick implements Listener {
 	
 	@EventHandler
 	public void onInventoryClickEvent(InventoryClickEvent e) {
-		Player player = (Player) e.getWhoClicked();
-		
 		if (e.getInventory().getHolder() == null) {
-			player.sendMessage("Grabbing items...");
 			e.setCancelled(true);
 		}
 	}
